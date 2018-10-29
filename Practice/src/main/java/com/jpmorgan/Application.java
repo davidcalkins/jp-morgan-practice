a228d0cb7125b3bed264c73a227030310a94fc93
davidcalkins <david.calkins.1000@gmail.com>
2018-10-29 19:04:48 -0400

    Created Project with basic pom dependencies

---
+++ Practice/src/main/java/com/jpmorgan/Application.java
@@ -0,0 +1,16 @@
+package com.jpmorgan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
